820e3306523a52b86bcbf7a0084b60194a3a0a29
taroh3 <taroh3333+github@gmail.com>
2026-06-30 13:16:42 +0900

    Update 60_live_gallery_sw.js

--- 60_live_gallery_sw.js
+++ 60_live_gallery_sw.js
@@ -1,8 +1,9 @@
 // 60_live_gallery_sw.js
 // たろうのLive Gallery Service Worker
-// v6.0.0
+// v7.0.0
 //
 // 変更履歴：
+// v7.0.0 - 写真リンクにnoreferrer追加テスト
 // v6.0.0 - 年選択時に年始の公演を表示
 // v5.0.0 - ポスター長押しライトボックス追加
 // v4.0.0 - アーティスト写真を上詰め表示に変更
@@ -10,7 +11,7 @@
 // v2.0.0 - キャッシュ名をv2に更新・対象HTMLを60_live_gallery.htmlに修正
 // v1.0.0 - 初版作成
 
-var CACHE_NAME    = 'live-gallery-v6';
+var CACHE_NAME    = 'live-gallery-v7';
 var STATIC_ASSETS = [
   './',
   './60_live_gallery.html',
